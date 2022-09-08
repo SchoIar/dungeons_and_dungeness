cbe1f7413f03d9ebcfae4ae8d8c53e4e461a80a6
Anton Ilic <93786227+SchoIar@users.noreply.github.com>
2022-09-08 09:15:34 -0700

    added basic functionality to game engine, map.

--- main.py
+++ main.py
@@ -2,26 +2,66 @@ from sys import exit
 from random import randint
 import time
 import math
+
+# Class Hierarchy
+# * Map
+#   - next_scene
+#   - opening_scene
+# * Engine
+#   - play
+# * Scene
+#   - enter
+#   * Death
+# #Add more scenes later....
+
 class Scene(object):
     def enter(self):
-        pass
+        print("This scene is not yet configured.")
+        exit(1)
+        #this is what happens for all scenes
 
 class Engine(object):
-    def __init__(self):
-        pass
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+        # be sure to print out the last scene
+        current_scene.enter()
 
-class Death():
+class Death(Scene):
     def enter(self):
-        pass
+        print('You died. ')
+        exit(1)
 
+class Start(Scene):
+    def enter(self):
+        print('You enter a room... [...]')
+        print('\n You can do the following: 1. Run 2. Fight')
+        action = input("> ")
+        if action == "Run" or "1":
+            print('You run into an even bigger crab, which eats you.')
+            return 'death'
 
 class Map(object):
+    scenes = {
+        'start': Start(),
+        'death': Death(),
+    }
+
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        pass
+        return Map.scenes.get(scene_name)
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
 
 
+a_map = Map('start')
+a_game = Engine(a_map)
+a_game.play()
