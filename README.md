0a1f30094b64e8599f3bcd13f342c0e3d3233fb3
Anton Ilic <93786227+SchoIar@users.noreply.github.com>
2022-09-08 08:41:43 -0700

    added basic framework for game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dungeons_and_dungeness
+
+A game based on Dungeons & Dragons concept with a twist: all your enemies are dungeness crabs. 
---
+++ main.py
@@ -0,0 +1,23 @@
+class Scene(object):
+    def enter(self):
+        pass
+
+class Engine(object):
+    def __init__(self):
+        pass
+    def play(self):
+        pass
+
+class Death():
+    def enter(self):
+        pass
+
+
+class Map(object):
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    
